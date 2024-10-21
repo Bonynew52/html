8a851505238145b65bec468f8c9f0831979cce0b
Bonynew52 <116922615+Bonynew52@users.noreply.github.com>
2024-10-21 16:23:43 +0000

    dss

--- html-test/src/app/imagen/page.tsx
+++ html-test/src/app/imagen/page.tsx
@@ -10,7 +10,6 @@ export default function Page() {
         <Image src={Chrome} alt="chromium logo" className="w-32" />
         <form action="https://www.google.com/search" className="flex flex-col items-center w-2/5 space-y-8">
           <input type="text" name="q" className="border-2 border-gray-600 w-full" />
-        
           <div className="flex flex-row justify-center space-x-4 ">
             <button type="submit" className="w-40 border-2 border-gray-600 bg-gray-800 text-white">Google search</button>
             <button type="submit" className="w-40 border-2 border-gray-600 bg-gray-800 text-white">I'm feeling lucky</button>
